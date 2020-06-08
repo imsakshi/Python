2a1c70080242099b4d54d59464c820ee5f1e454d
Sakshi Manga <sakshi.manga@gmail.com>
2020-06-08 13:48:36 +0530

    Add files via upload

---
+++ kalci.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(a,b):
+    return a + b
+
+
+# In[2]:
+
+
+def mul(a,b):
+    return a * b
+
+
+# In[3]:
+
+
+def div(a,b):
+    return a/b
+
+
+# In[ ]:
+
+
+
+
